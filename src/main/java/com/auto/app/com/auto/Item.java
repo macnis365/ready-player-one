a9f9cb7888b2c60cfa786d39bf9dbe688352e790
micheal <macnis365@users.noreply.github.com>
2019-01-23 17:16:30 +0530

    Create Item.java

---
+++ src/main/java/com/auto/app/com/auto/Item.java
@@ -0,0 +1,53 @@
+package com.auto.app.com.auto.game;
+
+public class Item {
+    private String name;
+    private int points;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public static class ItemBuilder {
+        private String name;
+        private int points;
+
+        public ItemBuilder() {
+
+        }
+
+        public ItemBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ItemBuilder withPoints(int points) {
+            this.points = points;
+            return this;
+        }
+
+        public Item build() {
+            Item item = new Item();
+            item.name = this.name;
+            item.points = this.points;
+            return item;
+        }
+    }
+
+    private Item() {
+
+    }
+}
