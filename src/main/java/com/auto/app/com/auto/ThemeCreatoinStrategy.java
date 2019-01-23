d38c3515b4377eb436a7346e1a54cc1ad49d187b
micheal <macnis365@users.noreply.github.com>
2019-01-23 17:17:46 +0530

    Create ThemeCreatoinStrategy.java

---
+++ src/main/java/com/auto/app/com/auto/ThemeCreatoinStrategy.java
@@ -0,0 +1,9 @@
+package com.auto.app.com.auto.game.themestrategy;
+
+import com.auto.app.com.auto.game.Theme;
+
+import java.util.Scanner;
+
+public interface ThemeCreatoinStrategy {
+    public Theme createTheme(Scanner input);
+}
