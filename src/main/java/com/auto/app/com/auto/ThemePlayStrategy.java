fbff1eb6b6d929837339b53120987f283b56f705
micheal <macnis365@users.noreply.github.com>
2019-01-23 17:18:27 +0530

    Create ThemePlayStrategy.java

---
+++ src/main/java/com/auto/app/com/auto/ThemePlayStrategy.java
@@ -0,0 +1,9 @@
+package com.auto.app.com.auto.game.themestrategy;
+
+import com.auto.app.com.auto.game.Theme;
+
+import java.util.Scanner;
+
+public interface ThemePlayStrategy {
+    public void play(Theme theme, Scanner input);
+}
